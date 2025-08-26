31e3c2c6cc5965c2ffe8272c505d8e5bc298990c
lakshanjayalath <iamdhananjaya119@gmail.com>
2025-08-26 22:16:52 +0530

    Add MongoDB connection setup and update dependencies in package.json

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, {Application} from "express";
+import express, { Application } from "express";
 import { Routes } from "./routes/routes";
 import { APP_CONFIG } from "./config/app.config";
-// import greetingRouter from "./routes/greeting.route";
+import { MongoLoader } from "./loader/mongo.loader";
 
-const app: Application = express();
+const app:Application = express();
 
 app.use(express.json());
-app.use("/api", Routes.getInstance().router);
-
+app.use("/api" ,Routes.getInstance().router)
 
 const PORT = APP_CONFIG.PORT;
-
-app.listen(PORT, () =>{
+const mongoLoader = new MongoLoader();
+mongoLoader.connect();
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
