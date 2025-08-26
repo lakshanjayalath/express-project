823518ea57609276be2ce7f139195cab1c7e4647
lakshanjayalath <iamdhananjaya119@gmail.com>
2025-08-26 18:02:10 +0530

    Add configuration management with dotenv and update app to use dynamic port

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, {Application} from "express";
 import { Routes } from "./routes/routes";
+import { APP_CONFIG } from "./config/app.config";
 // import greetingRouter from "./routes/greeting.route";
 
 const app: Application = express();
@@ -8,7 +9,7 @@ app.use(express.json());
 app.use("/api", Routes.getInstance().router);
 
 
-const PORT = 3000;
+const PORT = APP_CONFIG.PORT;
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
